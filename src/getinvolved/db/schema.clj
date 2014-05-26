4e07a3328ea5c4cf2f28fc4ed16f9423ec0be916
Jeff Taggart <jeff@jetaggart.com>
2014-05-26 16:50:17 -0600

    Begin clojure app

---
+++ src/getinvolved/db/schema.clj
@@ -0,0 +1,9 @@
+(ns getinvolved.db.schema)
+
+(def db-spec
+  {:subprotocol "postgresql"
+   :subname "//localhost/getinvolved"
+   :user "db_user_name_here"
+   :password "db_user_password_here"})
+
+
