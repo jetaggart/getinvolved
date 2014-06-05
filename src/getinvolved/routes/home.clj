bf64f716f037c63d51ee260e2b7eb396a5655876
Jeff Taggart <jeff@jetaggart.com>
2014-06-04 22:02:08 -0600

    Got hero almost right

--- src/getinvolved/routes/home.clj
+++ src/getinvolved/routes/home.clj
@@ -4,7 +4,29 @@
             [getinvolved.util :as util]))
 
 (defn home-page []
-  (layout/render [:p "something"]))
+  (layout/render
+    [:div.hero
+     [:div.hero-inner
+      [:a.hero-logo {:href "javascript:void(0)"}
+       [:img {:alt "hero" :src "https://raw.github.com/Magnus-G/Random/master/placeholder_logo_1.png"}]]
+      [:div.hero-copy
+       [:h1 "Get Involved"]
+       [:p "Want to contribute more than money? Find a story!"]]
+      [:button "Learn More"]]]
+    [:section
+     [:div.bullets
+      [:a.bullet.two-col-bullet {:href "/users/new"}
+       [:div.bullet-icon.bullet-icon-1
+        [:img {:src "https://raw.github.com/Magnus-G/Random/master/placeholder_logo_2.png"}]]
+       [:div.bullet-content
+        [:h2 "Begin a Story"]
+        [:p "Lorem dolor sit amet consectetur adipisicing elit. Doloremque, minus, blanditiis, voluptatibus nulla quia ipsam sequi quos iusto aliquam iste magnam accusamus molestias quo illum impedit. Odit officia autem."]]]
+      [:a.bullet.two-col-bullet
+       [:div.bullet-icon.bullet-icon-2
+        [:img {:src "https://raw.github.com/Magnus-G/Random/master/placeholder_logo_3.png"}]]
+       [:div.bullet-content
+        [:h2 "Contribute to a story"]
+        [:p "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, minus, blanditiis, voluptatibus nulla quia ipsam sequi quos iusto aliquam iste magnam accusamus molestias quo illum."]]]]]))
 
 (defroutes home-routes
-  (GET "/" [] (home-page)))
+           (GET "/" [] (home-page)))
